58dae06c65e319f8e98a27bb864970bff50cac68
James Hagan <james_hagan@outlook.com>
2020-09-17 11:21:52 +0200

    clean up Manning et al. (2018) function

--- Scripts/MF_manning_2018.R
+++ Scripts/MF_manning_2018.R
@@ -1,17 +1,96 @@
 
-# Manning et al. (2018): Clustering-based multifunctionality
+# function to calculate multifunctionality as proposed by Manning et al. (2018, Nature Ecology and Evolution)
+
+# adf, is dataframe with plots in rows, and functions in columns
+# vars has to bee a named vector of functions to include which has to correspond to column names
+# ind is the index used to determine the optimal number of clusters using NbClust function (if multiple are included, the median of all of them is used)
+# met is the method used for clustering. This functions supports all methods in hclust() besides "kmeans"
+# thresh is the threshold for function assigngin a zero or one
+
+library(NbClust)
+
+manning_multifunc <- function(adf, vars, 
+                              ind = c("mcclain", "cindex", "silhouette", "dunn"), 
+                              met = "ward.D2",
+                              dis = "euclidean",
+                              thresh = 0.5) {
+  
+  if(! "NbClust" %in% installed.packages()[,1]) stop(
+    "this function requires NbClust to be installed and loaded"
+  )
+  
+  if(length(vars) <= 1) stop(
+    "this function requires at least two ecosystem functions"
+  )
+  
+  # extract functions from input matrix: adf
+  adf_mat <- adf[, vars] 
+  
+  # transpose the adf matrix
+  adf_mat_t <- as.data.frame(t((adf_mat[ , ]))) 
+  
+  # generate a distance matrix based on the transposed adf matrix
+  adf_dist <- dist(adf_mat_t, method = dis)
+  
+  # from the distance matrix, use NbClust to find optimal number of clusters
+  opt_clus <- vector(length = length(ind))
+  for(i in 1:length(ind)) {
+    
+    x <- NbClust::NbClust(diss = adf_dist, 
+                          distance = NULL, 
+                          min.nc = 2, max.nc = (ncol(adf_mat) - 1), 
+                          method = met, index = ind[i])
+    
+    opt_clus[i] <- x$Best.nc[1]
+    
+  }
+  
+  # take the median number of clusters based on the four indices computed from NbClust
+  clus_n <- round(x = median(opt_clus), digits = 0)
+  
+  # use hclust() to cluster the functions
+  adf_den <- hclust(func_dist, method = met )
+  
+  # based on median number of clusters, divide the tree into groups
+  adf_groups <- cutree(adf_den, k = clus_n)
+  
+  # function loadings
+  func_loads <- 
+    unlist(lapply(adf_groups, FUN = function(x) { ( (1)/length(adf_groups[adf_groups == x]) ) } ))
+  
+  # standardise functions by maximum and assign based on thresholds
+  adf_thres <- 
+    apply(X = adf_mat, MARGIN = 2,
+          FUN = function(z) { 
+            
+            y <- z/max(z)
+            
+            (ifelse(y > thresh, 1, 0))
+            
+          } )
+  
+  # multiply this by the function loads based on the cluster analysis
+  adf_thres <- sweep(adf_thres, MARGIN = 2, func_loads, `*`)
+  
+  # add the multifunctionality score to the input data
+  adf$manning_mf <-  rowSums(adf_thres)/ncol(adf_mat)
+  
+  return(adf)
+  
+}
+
+
+### test this function on the simulated data
 
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(here)
-library(NbClust)
 
+# where to get scripts for the simulated diversity experiment
 source(here("Scripts","Multifunctionality-Simulations", "Multifunc_simulations_functions.R"))
-source(here("Scripts", "MF_index_function.R"))
-source(here("Scripts", "MF_Hill_function.R"))
 
-### Simulate full diversity experiment
+# simulate full diversity experiment
 set.seed(777)
 
 specnum <- 10
@@ -41,80 +120,16 @@ set.seed(563)
 errM <- matrix(rnorm(n = nrow(AvFunc)*funcnum, mean = 0, sd = 0.01), ncol = funcnum)
 
 #add variance
-AvFunc[,func.names] <- AvFunc[,func.names] + errM
+AvFunc[,func.names] <- AvFunc[, func.names] + errM
 
-# standardize functions 
-AvFunc_func <- 
-  AvFunc %>% 
-  mutate_at(vars(one_of(func.names)), function(x) {(x) / max(x)})
+# run the function on these simulated data
+manning_multifunc(adf = AvFunc,
+                  vars = func.names,
+                  ind = c("mcclain", "cindex", "silhouette", "dunn"), 
+                  met = "ward.D2",
+                  dis = "euclidean",
+                  thresh = 0.5)
 
-
-# get a matrix of functions
-func_matrix <- AvFunc_func[ , func.names]
-
-
-# cluster analysis of the functions
-
-# first, we transpose the data matrix because we want to examine similarity among functions
-func_matrix_inv <- as.data.frame(t((func_matrix[ , ])))
-
-# create a distance matrix of the functions
-func_dist <- dist(func_matrix_inv, method = "euclidean")
-
-# we can plot these in a dendrogram
-
-# start the function
-
-# can use any method except: "kmeans" as this requires all the data
-
-# index = "frey" doesn't work for some reason
-
-
-adf <- func_matrix
-
-vars <- names(func_matrix)
-
-ind <- c("mcclain", "cindex", "silhouette", "dunn")
-
-met <- "ward.D2"
-
-dis <- "euclidean"
-
-
-# get functions from the adf matrix
-adf_mat <- adf[, vars]
-
-# transpose the adf_mat data
-adf_mat_t <- as.data.frame(t((adf_mat[ , ])))
-
-# generate a dissimilarity matrix from adf_mat_t
-adf_dist <- dist(adf_mat_t, method = dis)
-
-# use the dissimilarity matrix to determine optimum group number for cluster analysis
-
-opt_clus <- vector(length = length(ind))
-  
-for(i in 1:length(ind)) {
-  
-  x <- NbClust(diss = adf_dist, 
-               distance = NULL, 
-               min.nc = 2, max.nc = (ncol(adf_mat) - 1), 
-               method = met, index = ind[i])
-  
-  opt_clus[i] <- x$Best.nc[1]
-  
-}
-
-clus_n <- round(x = median(opt_clus), digits = 0)
-
-adf_den <- hclust(func_dist, method = met )
-
-adf_groups <- cutree(adf_den, k = clus_n)
-adf_groups
-
-# function loadings
-func_loads <- 
-  unlist(lapply(adf_groups, FUN = function(x) { ( (1)/length(adf_groups[adf_groups == x]) ) } ))
 
 
 
