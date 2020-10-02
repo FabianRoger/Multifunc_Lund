41bf05efb43a47843bcfc219c332c98d447b75bd
James Hagan <james_hagan@outlook.com>
2020-10-02 13:12:16 +0200

    remove the slope function

--- Scripts/Multifunctionality-Simulations/Multifunc_simulations_functions.R
+++ Scripts/Multifunctionality-Simulations/Multifunc_simulations_functions.R
@@ -644,75 +644,7 @@ is.positive.definite <- function( x, tol=1e-8 )
   return( TRUE )
 }
 
-# function to get the slope for different numbers of functions in a dataset
-# data is a dataset with multiple functions as columns
-# funcs is a vector of function names
-est_n_func <- function(data, funcs) {
-  
-  s_est <- function(a, b, d) {
-    
-    # set up combinations for each number of functions
-    comb_f <- gtools::combinations(n = length(b), r = d)
-    
-    # set up an output vector
-    y <- vector("list", length = nrow(comb_f))
-    
-    for (i in 1:nrow(comb_f)) {
-      
-      v <- b[comb_f[i, ]]
-      
-      df <- a[, v]
-      
-      lm_df <- 
-        a %>%
-        mutate(species_pool = scale(species_pool, scale = TRUE, center = TRUE),
-               ave_mf = MF_av(adf = df, vars = v),
-               sum_mf = MF_sum(adf = df, vars = v),
-               pasari_mf = MF_pasari(adf = df, vars = v),
-               enf_mf = hill_multifunc(adf = df, vars = v, scale = 1, HILL = TRUE),
-               thresh_30_mf = single_threshold_mf(adf = df, vars = v, thresh = 0.3),
-               thresh_70_mf = single_threshold_mf(adf = df, vars = v, thresh = 0.7))
-      
-      mf_names <- names(select(lm_df, ends_with("mf")))
-      
-      s_cof <- vector(length = length(mf_names))
-      
-      for(s in 1:length(mf_names)) {
-        
-        lm1 <- lm(as.formula(paste0(mf_names[s], "~ species_pool")),
-                  data = lm_df)
-        
-        s_cof[s] <- lm1$coefficients[2]
-        
-      }
-      
-      names(s_cof) <- c(mf_names)
-      
-      y[[i]] <- s_cof
-      
-    } 
-    
-    y <- bind_rows(y, .id = "run")
-    
-    y$number_functions <- d
-    
-    y
-    
-  }
-  
-  # loop over this function for all possible numbers of functions
-  slopes_n_func <- vector("list", length = length(funcs))
-  
-  for(j in 2:length(funcs)) {
-    
-    slopes_n_func[[j]] <- 
-      s_est(a = data, b = funcs, d = j)
-    
-  }
-  
-  bind_rows(slopes_n_func, .id = "n_func")
-  
-}
+
 
 
 
