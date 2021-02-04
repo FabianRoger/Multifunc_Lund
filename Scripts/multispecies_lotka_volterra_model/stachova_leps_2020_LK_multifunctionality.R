b8e88f72bd837fee33aa4cfb617aa46f8c115af8
James Hagan <james_hagan@outlook.com>
2021-02-04 12:12:12 +0100

    update simulation script

--- Scripts/multispecies_lotka_volterra_model/stachova_leps_2020_LK_multifunctionality.R
+++ Scripts/multispecies_lotka_volterra_model/stachova_leps_2020_LK_multifunctionality.R
@@ -6,6 +6,7 @@
 # select scripts to call functions from
 library(here)
 source(here("Scripts/multispecies_lotka_volterra_model/stachova_leps_2010_LK_model_function.R"))
+source(here("Scripts/MF_functions_collated.R"))
 
 # equal competition
 # stabilising competition
@@ -82,16 +83,14 @@ l <- rweibull(n = 1000, shape = w.shape, scale = w.scale)
 hist(l)
 mean(l)
 
-prob.neg <- 0.3
-prob.pos <- 0.7
+prob.neg <- 0.2
 
 func.mat <- 
   lapply(spp.list, function(x){
-  # x <- rweibull(n = func.n, shape = w.shape, scale = w.scale)
-    x <- rnorm(n = func.n, mean = 0.5, sd = 0.1)
-  y <- sample(x = c(-1, 1), size = func.n, prob = c(prob.neg, prob.pos), replace = TRUE)
-  z <- (x*y)
-  round(z, digits = 4)
+    x <- rweibull(n = func.n, shape = w.shape, scale = w.scale)
+    y <- x - quantile(x, prob.neg)
+    z <- (x*y)
+    round(z, digits = 4)
 })
 
 func.mat <- data.frame(do.call(rbind, func.mat))
@@ -113,18 +112,68 @@ func.mat %>%
   theme_bw() +
   theme(legend.position = "none")
 
-
-
 multi.func <- full_join(df.spp, func.mat, by = "species")
 
 # multiply abundance by these function values
+multi.func <- 
+  multi.func %>%
+  mutate(across(.cols = starts_with(match = "F_"), ~(.*abundance) ))
+
+# function to standardise and then translate the functions
+func.std.trans <- function(x){
+  y <- (x - mean(x))/sd(x)
+  return(y + abs(min(y)))
+}
+
+multi.func <- 
+  multi.func %>%
+  group_by(patch) %>%
+  summarise(across(.cols = starts_with("F_"), sum), .groups = "drop") %>%
+  mutate(across(.cols = starts_with("F_"), func.std.trans ))
+
+
+# calculate the MF metrics on these datasets with different levels of correlation
+func.names <- names(multi.func)
+func.names <- func.names[grepl(pattern = "F_", func.names)]
+
+x <- select(multi.func, func.names)
+
 multi.func %>%
-  mutate(across(.cols = starts_with(match = "F_"), ~(.*abundance) )) %>%
-  pivot_longer(cols = starts_with(match = "F_"),
-               names_to = "function",
-               values_to = "val") %>%
-  ggplot(data = .,
-         mapping = aes(x = ))
+  mutate(`scal. MF` = MF_jing(adf = x, vars = func.names),
+         `sum MF` = MF_sum(adf = x, vars = func.names),
+         `ave. MF` = MF_av(adf = x, vars = func.names),
+         `MESLI MF` = MF_mesli(adf = x, vars = func.names),
+         `Pasari MF` = MF_pasari(adf = x, vars = func.names),
+         `SAM MF` = MF_dooley(adf = x, vars = func.names),
+         `ENF MF` = hill_multifunc(adf = x, vars = func.names, scale = 1, HILL = TRUE),
+         `Simp. MF` = MF_simpsons_div(adf = x, vars = func.names),
+         `Manning.30 MF` = manning_multifunc(adf = x, vars = func.names, thresh = 0.3),
+         `Manning.50 MF` = manning_multifunc(adf = x, vars = func.names, thresh = 0.5),
+         `Manning.70 MF` = manning_multifunc(adf = x, vars = func.names, thresh = 0.7),
+         `thresh.30 MF` = single_threshold_mf(adf = x, vars = func.names, thresh = 0.3),
+         `thresh.50 MF` = single_threshold_mf(adf = x, vars = func.names, thresh = 0.5),
+         `thresh.70 MF` = single_threshold_mf(adf = x, vars = func.names, thresh = 0.7),
+         `Slade.10.90 MF` = MF_slade(adf = x, vars = func.names, A_quant = 0.10, B_quant = 0.90),
+         `Slade.40.60 MF` = MF_slade(adf = x, vars = func.names, A_quant = 0.40, B_quant = 0.60),
+         `PCA MF` = pca_multifunc(adf = x, vars = func.names, standardise = FALSE) ) %>%
+  View()
+  
+
+# MF metric groups
+MF_groups <- 
+  c(rep("sum/ave.", 4),
+    rep("evenness", 4),
+    rep("threshold", 8),
+    rep("other", 1))
+
+
+
+
+
+
+
+
+
 
 
 
