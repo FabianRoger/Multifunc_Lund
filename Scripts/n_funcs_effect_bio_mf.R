64805e5af731861149edcb10332baa21a0fa89d5
James Hagan <james_hagan@outlook.com>
2021-02-08 14:02:50 +0100

    define functions to get all possible function combinations from a vector of function names

--- Scripts/n_funcs_effect_bio_mf.R
+++ Scripts/n_funcs_effect_bio_mf.R
@@ -10,6 +10,9 @@ library(tidyr)
 library(ggplot2)
 library(here)
 
+# load functions from important scripts
+source(here("Scripts/MF_functions_collated.R"))
+
 # load the cleaned Jena data
 jena.dat <- read_csv(file = here("data/jena_data_cleaned.csv"))
 
@@ -29,12 +32,53 @@ func.names <- var.names[!(var.names %in% ( c(spp.names, site.id) ))  ]
 
 # BEF slope and n-functions
 
-n.funcs <- 1:length(func.names)
-
-function.combinations <- vector("list", length = length(func.names))
-for (i in n.funcs){
-  function.combinations[[i]] <- combn(x = func.names, m = i)
+# get the list of matrices of function combinations
+function.combinations <- function(vector.func.names) {
+  func.combs <- vector("list", length = length(func.names))
+  for (i in n.funcs){
+    func.combs[[i]] <- combn(x = vector.func.names, m = i)
+  }
+  return(func.combs)
 }
-function.combinations[[9]]
+
+# write a function to flatten an individual part of a nested list
+flatten.list.matrices <- function(nested.list){
+  unnested.list <- split(nested.list, col(nested.list)) 
+  names(unnested.list) <- NULL 
+  return(unnested.list)
+}
+
+# combine function.combinations and flatten.list.matrices and loop over each n-func
+get.function.combinations <- function(function.names){
+  
+  # get list of matrices with function combinations
+  list.func.matrix <- function.combinations(vector.func.names = function.names)
+  
+  # flatten the first matrix in the list
+  list.combination <- flatten.list.matrices(nested.list = list.func.matrix[[1]])
+  
+  # loop over this and bind into a list
+  for (i in 2:length(list.func.matrix)){
+    x <- flatten.list.matrices(nested.list = list.func.matrix[[i]])
+    list.combination <- c(list.combination, x)
+  }
+  return(list.combination)
+  
+}
+
+# use the function to get a list of all function combinations
+get.function.combinations(function.names = func.names)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
