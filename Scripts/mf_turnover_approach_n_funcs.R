847d3c88fda6e3b08e365d53f8c93683932c4f82
James Hagan <james_hagan@outlook.com>
2021-02-18 11:45:55 +0100

    wrap the aic-based species effect code and the Gotelli et al. (2011) species effect code into two separate functions

--- Scripts/mf_turnover_approach_n_funcs.R
+++ Scripts/mf_turnover_approach_n_funcs.R
@@ -6,7 +6,6 @@
 # install and load the multifunc package
 # library(devtools)
 # install_github("jebyrnes/multifunc")
-library(multifunc)
 
 # arguments for the functions:
 
@@ -148,6 +147,7 @@ turnover_aic_null <- function(func.names,
 }
 
 # test the turnover_aic_null function using BIODEPTH data
+library(multifunc)
 data(all_biodepth)
 allVars <- qw(biomassY3, root3, N.g.m2,  light3, N.Soil, wood3, cotton3)
 
@@ -276,31 +276,147 @@ func.dat <- func.dat[func.dat$species %in% spp.present, ]
 any(func.dat$species != spp.present)
 
 
-# write two functions:
 
-# (1) calculate species effects using the AIC-based turnover approach
+# functions to implement aic-based species effects and Gotelli et al. (2011) species effects
 
-# (2) calculate species effects using Gotelli et al. (2011)'s method
+# 1. calculate species effects using the AIC-based turnover approach
 
-a.out <- vector("list", length = length(f.names))
-pa.out <- vector("list", length = length(f.names))
-for (i in 1:length(f.names)){
+# data: data.frame with functions and either species abundances or presence-absences as columns and samples as rows
+# function_names: vector of function names
+# species_names: vector of species names
+
+AIC_sp <- function(data, function_names, species_names) {
   
-  # get species effect on each function using abundances
-  # this outputs a vector of species effects (-1, 0 or 1) on the function i
-  r.abun <- getRedundancy(vars = f.names[i], species = spp.present, data = mf.a)
-  a.out[[i]] <- sapply(r.abun, function(x)(x))
+  if(! "multifunc" %in% installed.packages()[,1]) stop(
+    "this function requires the multifunc package to be installed"
+  )
   
-  # get species effect on each function using presence-absence
-  # this outputs a vector of species effects (-1, 0 or 1) on the function i
-  r.pa <- getRedundancy(vars = f.names[i], species = spp.present, data =  mf.pa)
-  pa.out[[i]] <- sapply(r.pa, function(x)(x))
+  # load the multifunc package
+  library(multifunc)
   
+  sp.effect.aic <- vector("list", length = length(function_names))
+  for (i in 1:length(function_names)){
+    
+    # get species effect on each function using abundances
+    # this outputs a vector of species effects (-1, 0 or 1) on the function i
+    redun.out <- getRedundancy(vars = function_names[i], species = species_names, data = data)
+    sp.effect.aic[[i]] <- sapply(redun.out, function(x)(x))
+    
   }
+  
+  # prepare the output
+  sp.effect.aic <- as.data.frame(do.call(cbind, sp.effect.aic))
+  names(sp.effect.aic) <- function_names
+  sp.effect.aic <- cbind(species = rownames(sp.effect.aic), data.frame(sp.effect.aic, row.names = NULL))
+  
+  # return the data.frame with species effects
+  return(sp.effect.aic)
+  
+}
 
-do.call(cbind, a.out)
-do.call(cbind, pa.out)
+# test this function
+y <- AIC_sp(data = mf.pa, function_names = f.names, species_names = spp.present)
 
+
+# 2. calculate species effects using Gotelli et al. (2011)'s method
+
+# data: data.frame with functions and species presence-absences as columns and samples as rows
+# function_names: vector of function names
+# species_names: vector of species names
+# n_ran: number of randomisations
+
+SES_score <- function(data, function_names, species_names, n_ran) {
+  
+  if(! "dplyr" %in% installed.packages()[,1]) stop(
+    "this function requires the dplyr package to be installed"
+  )
+  if(! "tidyr" %in% installed.packages()[,1]) stop(
+    "this function requires the tidyr package to be installed"
+  )
+  
+  # load dplyr and tidyr packages
+  library(dplyr)
+  library(tidyr)
+  
+  # randomise the data and calculate d-values for each randomisation
+  ran.d.vals <- vector("list", length = n_ran)
+  for (i in 1:n_ran) {
+    
+    # randomise function values across plots
+    data.random <- row.randomiser(func.names = function_names,
+                                  species.names = species_names, 
+                                  adf.data = data)
+    
+    # calculate difference values for each species for each function
+    ran.d.vals[[i]] <-  
+      data.random %>%
+      pivot_longer(cols = all_of(species_names),
+                   names_to = "species",
+                   values_to = "pa") %>%
+      group_by(species, pa) %>%
+      summarise(across(.cols = all_of(function_names), mean )) %>%
+      summarise(across(.cols = all_of(function_names), diff ), .groups = "drop")
+    
+  }
+  
+  # bind the list into a data.frame  
+  ran.d.vals <- bind_rows(ran.d.vals, .id = "run")
+  
+  # calculate mean and sd of d-values of randomised data
+  ran.d.vals <- 
+    ran.d.vals %>%
+    pivot_longer(cols = all_of(function_names),
+                 names_to = "function_name",
+                 values_to = "d_value") %>%
+    arrange(species, function_name) %>%
+    group_by(species, function_name) %>%
+    summarise(mean_d_value = mean(d_value, na.rm = TRUE),
+              sd_d_value = sd(d_value, na.rm = TRUE), .groups = "drop")
+  
+  # calculate observed d values
+  obs.d.vals <- 
+    data %>%
+    pivot_longer(cols = all_of(species_names),
+                 names_to = "species",
+                 values_to = "pa") %>%
+    group_by(species, pa) %>%
+    summarise(across(.cols = all_of(function_names), mean )) %>%
+    summarise(across(.cols = all_of(function_names), diff ), .groups = "drop") %>%
+    pivot_longer(cols = starts_with("F_"),
+                 names_to = "function_name",
+                 values_to = "d_value_obs") %>%
+    arrange(species, function_name)
+  
+  # join the observed d.vals to the randomised d.vals
+  d.val.dat <- full_join(ran.d.vals, obs.d.vals, by = c("species", "function_name"))
+  
+  # calculate species importance scores
+  sp.SES <- 
+    d.val.dat %>%
+    mutate(SES = (d_value_obs - mean_d_value)/(sd_d_value)) %>%
+    mutate(SES_effect = if_else(SES > 2, 1, 
+                                if_else(SES < -2, -1, 0))) %>%
+    select(species, function_name, SES_effect) %>%
+    pivot_wider(id_cols = "species",
+                names_from = "function_name",
+                values_from = "SES_effect")
+  
+  return(sp.SES)
+  
+}
+
+x <- SES_score(data = mf.pa, function_names = f.names, species_names = spp.present, n_ran = 100)
+
+
+# we have the x and y data
+# compare to each other and to the true results
+
+y$F_1
+x$F_1
+
+
+
+### quantify deviation from true results and compare them
 
 # proportion of incorrect directions?
 # true.function.vals: real function values (i.e. linear coefficient)
@@ -329,89 +445,6 @@ error.abun <- compare.directions.vectors(true.function.vals = f.vals, inferred.e
 
 # ifelse(f.vals < 0, -1, 1)[r.abun != 0]
 # r.abun[r.abun != 0]
-
-
-### implement Gotelli et al. (2011)'s turnover metric
-
-# use row.randomiser to reshuffle function rows
-# then calculate D-values for the shuffled data
-
-# set number of replicates
-n <- 1000
-
-# data: species presence-absence data
-
-SES_score <- function(data, function_names, species_names, n_ran) {
-  
-  ran.d.vals <- vector("list", length = n_ran)
-  for (i in 1:n_ran) {
-    
-    # randomise function values across plots
-    data.random <- row.randomiser(func.names = function_names,
-                                  species.names = species_names, 
-                                  adf.data = data)
-    
-    # calculate difference values for each species for each function
-    ran.d.vals[[i]] <-  
-      data.random %>%
-      pivot_longer(cols = species_names,
-                   names_to = "species",
-                   values_to = "pa") %>%
-      group_by(species, pa) %>%
-      summarise(across(.cols = all_of(function_names), mean ), .groups = "keep") %>%
-      summarise(across(.cols = all_of(function_names), diff ), .groups = "drop")
-    
-  }
-  
-  # bind the list into a data.frame  
-  ran.d.vals <- bind_rows(ran.d.vals, .id = "run")
-  
-  # calculate mean and sd of d-values of randomised data
-  ran.d.vals <- 
-    ran.d.vals %>%
-    pivot_longer(cols = function_names,
-                 names_to = "function_name",
-                 values_to = "d_value") %>%
-    arrange(species, function_name) %>%
-    group_by(species, function_name) %>%
-    summarise(mean_d_value = mean(d_value, na.rm = TRUE),
-              sd_d_value = sd(d_value, na.rm = TRUE), .groups = "drop")
-  
-  # calculate observed d values
-  obs.d.vals <- 
-    data %>%
-    pivot_longer(cols = species_names,
-                 names_to = "species",
-                 values_to = "pa") %>%
-    group_by(species, pa) %>%
-    summarise(across(.cols = all_of(function_names), mean ), .groups = "keep") %>%
-    summarise(across(.cols = all_of(function_names), diff ), .groups = "drop") %>%
-    pivot_longer(cols = starts_with("F_"),
-                 names_to = "function_name",
-                 values_to = "d_value_obs") %>%
-    arrange(species, function_name)
-  
-  # join the observed d.vals to the randomised d.vals
-  d.val.dat <- full_join(ran.d.vals, obs.d.vals, by = c("species", "function_name"))
-  
-  # calculate species importance scores
-  sp.SES <- 
-    d.val.dat %>%
-    mutate(SES = (d_value_obs - mean_d_value)/(sd_d_value)) %>%
-    mutate(SES_effect = if_else(SES > 2, 1, 
-                                if_else(SES < -2, -1, 0))) %>%
-    select(species, function_name, SES_effect) %>%
-    pivot_wider(id_cols = "species",
-                names_from = "function_name",
-                values_from = "SES_effect")
-  
-  return(sp.SES)
-  
-}
-
-SES_score(data = mf.pa, function_names = f.names, species_names = spp.present, n_ran = 10)
-
-
 
 
 
