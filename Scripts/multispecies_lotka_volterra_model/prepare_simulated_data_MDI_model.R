fd20fe55005dd6dfeae205cf20fbd276e0dad92d
James Hagan <james_hagan@outlook.com>
2021-02-26 15:17:55 +0100

    update preparation script instructions

--- Scripts/multispecies_lotka_volterra_model/prepare_simulated_data_MDI_model.R
+++ Scripts/multispecies_lotka_volterra_model/prepare_simulated_data_MDI_model.R
@@ -6,16 +6,45 @@ library(dplyr)
 library(tidyr)
 library(readr)
 library(ggplot2)
+
+# I always use relative paths using the package 'here'
+# if you are not using an R-project
+# the best thing to do for this code to run is is to set your working directory now
+setwd(dir = "")
+
+# then load the here package
 library(here)
 
+# then we make a folder called data in the current working directory
+if(! dir.exists(here("data"))){
+  dir.create(here("data"))
+}
+
+# add the two full datasets that I sent into this data folder
+# 1. multifunctionality_data.csv
+# 2. raw_abundance_data.csv
+
+# if you do not want to work with relative paths and here, 
+# just be sure to specify the correct paths when you read and write the data
+
+
+### load the raw data (if not using here() specify path manually)
+
 # import the .csv file called: 'multifunctionality_data.csv'
-mf.dat <- read_csv("./data/multifunctionality_data.csv")
+mf.dat <- read_csv(here("data/multifunctionality_data.csv"))
+
+# remove univariate multifunctionality metrics
+mf.dat <- 
+  mf.dat %>%
+  select(-ends_with("MF"))
 
 # import the .csv file called 'raw_abundance_data.csv'
-a.dat <- read_csv("./data/raw_abundance_data.csv")
+a.dat <- read_csv(here("data/raw_abundance_data.csv"))
 
 
-# view the data for one random simulation
+### view the data for one random simulation
+
+# you can run this code block (line 42-63) multiple times to see different simulations
 ran.sim <- 
   mf.dat %>%
   filter(sim.id == sample(unique(mf.dat$sim.id), 1))
@@ -39,6 +68,8 @@ ran.sim %>%
   theme_classic() +
   theme(legend.position = "bottom")
 
+
+### put data into format specified by Laura and Caroline
 
 # split a.dat into a list
 a.list <- split(a.dat, a.dat$sim.id)
@@ -87,6 +118,8 @@ z.test <-
 any(y.test != z.test[z.test > 0])
 
 
+### join the species abundance data to the function data
+
 # convert the multifunctionality data into a list
 mf.list <- split(mf.dat, mf.dat$sim.id)
 
@@ -99,20 +132,19 @@ mdi_list <-
 View(mdi_list[[1]])
 
 
-# save this list as an RDS file
+# save this list as an RDS file (I sent this file directly to both of you)
 
-# saveRDS(mdi_list, "./data/multi_div_model_list.rds")
-# l_load <- readRDS("./data/multi_div_model_list.rds") 
+# saveRDS(mdi_list, here("data/multi_div_model_list.rds"))
+# l_load <- readRDS(here("data/multi_div_model_list.rds")) 
 
 
 # code to output each simulated dataset (1:30) as a separate .csv into the current working directory
-
+# if not using here(), specify path manually in this loop
 for(i in 1:length(mdi_list)) {
   
   file_name <- paste("simulation_", i, sep = "")
-  
   write_csv(x = mdi_list[[i]], here("file_name"))
   
 }
 
-
+### END
