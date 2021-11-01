d094c8419a32e275f6b8b8bba0f4b5d0f1e307d9
James Hagan <james_hagan@outlook.com>
2021-11-01 13:24:54 +0100

    update cleaning script of the Jena data from Jochum et al. (2020)

--- Scripts/jena_analysis/clean_jena_data.R
+++ Scripts/jena_analysis/clean_jena_data.R
@@ -8,8 +8,12 @@ library(dplyr)
 library(tidyr)
 library(readr)
 library(ggplot2)
-
 library(here)
+
+# certain libraries must also be installed
+if(! "vegan" %in% installed.packages()[,1]) print(
+  "this script requires vegan to be installed"
+)
 
 # read in the Jena function data from Jochum et al. (2020)
 jena.func <- read_csv(here("data/J_avg_20190514.csv"))
@@ -42,61 +46,84 @@ absent_plots <- unique(jena.com$plotcode)[which( !(unique(jena.com$plotcode) %in
 jena.com %>%
   filter(plotcode %in% absent_plots) %>%
   View()
+rm(absent_plots)
 
 # get the plots used in the function data
 jena.com <- 
   jena.com %>%
   filter(plotcode %in% unique(jena.func$plotcode) )
 
-# subset out the relevant columns
-names(jena.com)
-spp <- names(jena.com)[52:100]
+# test if the subsetting worked: if FALSE, all plotcodes are equal
+any(sort(unique(jena.com$plotcode)) != sort(jena.func$plotcode) )
 
-# replace the NAs with zeros then summarise abundance across seasons
+# subset out the relevant columns
+
+# get a list of species names
+col_names <- names(jena.com)
+spp <- col_names[grepl(pattern = "[A-Z][a-z]{2}[.][a-z]{3}", col_names)]
+rm(col_names)
+
+# there should be 60 species in the dataset
+length(spp) == 60
+
+# summarise the percentage cover values by species across years
+jena.com %>%
+  select(all_of(spp)) %>%
+  View()
+
 jena.com <- 
   jena.com %>%
-  mutate(across(.cols = all_of(spp), ~if_else(is.na(.), 0, .))) %>%
   group_by(year, sowndiv, plotcode) %>%
-  summarise(across(.cols = spp, ~mean(.)), .groups = "drop") %>%
-  arrange(year, sowndiv, plotcode)
-head(jena.com)
+  summarise(across(.cols = all_of(spp), ~mean(., na.rm = TRUE)), .groups = "drop") %>%
+  arrange(year, sowndiv, plotcode) %>%
+  group_by(sowndiv, plotcode) %>%
+  summarise(across(.cols = all_of(spp), ~mean(., na.rm = TRUE)), .groups = "drop")
 
-# set-up a species by site matrix
-spp.site <- 
+# convert the NAs to 0's to indicate absence
+jena.com <- 
   jena.com %>%
-  select(all_of(spp))
+  mutate(across(.cols = all_of(spp), ~if_else(is.na(.), 0, . )))
 
-# subset out the id vars
-site <- 
-  jena.com %>%
-  select(-all_of(spp) )
+# join these data to the jena.func data
+# sort these data
+jena.func <- 
+  jena.func %>%
+  arrange(sowndiv, plotcode)
 
-# convert the spp.site to relative abundance
-spp.site <- spp.site/if_else(rowSums(spp.site) == 0, 1, rowSums(spp.site))
+# check if jena.com and jena.func match: if FALSE then they match
+any(jena.com$plotcode != jena.func$plotcode)
 
-# add realised diversity to the site data
-site$realised_diversity <- rowSums( vegan::decostand(spp.site, method = "pa") )
+# does the realised diversity calculated from jena.com match jena.func
+# yes, Pearson r is 0.99 so the community data are likely a good representatino
+plot(jena.func$S, rowSums(vegan::decostand(jena.com[, spp], method = "pa")) )
+cor.test(jena.func$S, rowSums(vegan::decostand(jena.com[, spp], method = "pa")) )
 
-# add the species abundances back to the site
-site <- bind_cols(site, spp.site)
+# add the calculated realised diversity to the jena.func data
+jena.func$realised_richness2 <- rowSums(vegan::decostand(jena.com[, spp], method = "pa"))
 
-# add the function data to the site data
-jena.dat <- 
-  full_join(site,
-            jena.func,
-            by = "plotcode")
+# join the species abundance data to the jena.func data
+jena.all <- full_join(jena.func, jena.com, by = c("sowndiv", "plotcode"))
 
-# remove plots with zero realised diversity and the 60 species plots
-jena.dat <- 
-  jena.dat %>%
-  filter(realised_diversity > 0) %>%
+# rename the S column
+jena.all <- 
+  jena.all %>%
+  rename(realised_richness1 = S)
+
+# reorder the columns
+names(jena.all)
+
+jena.all <- 
+  jena.all %>%
+  select(plotcode, block, sowndiv, realised_richness1, realised_richness2,
+         all_of(spp),
+         biomass, plantCN, soilC, soilorgC, herbi, micBMC, phoact, poll, rootBM)
+
+# remove the 60 species plots
+jena.all <- 
+  jena.all %>%
   filter(sowndiv < 60)
-head(jena.dat)
 
-# reflect the soil nutrient functions (i.e. low nutrients means high uptake)
-jena.dat <- 
-  jena.dat %>%
-  mutate(across(.cols = starts_with("Soil"), ~(.*-1) ))
+# output this cleaned data file
+write_csv(x = jena.all, file = here("data/jena_data_Jochum_2020_clean.csv"))
 
-# view the dataset
-View(jena.dat)
+### END
