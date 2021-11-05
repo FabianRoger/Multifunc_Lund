75632395cde4883c35453e6bd2744f20612ba104
James Hagan <james_hagan@outlook.com>
2021-11-05 15:56:53 +0100

    update the single function approach analysis

--- Scripts/single_function_approach/jena_single_function_illustration.R
+++ Scripts/single_function_approach/jena_single_function_illustration.R
@@ -31,7 +31,7 @@ jena.dat <-
 
 # choose four ecosystem functions to examine in the single function approach
 names(jena.dat)
-func.names <- c("biomass", "soilorgC", "poll", "rootBM")
+func.names <- c("biomass", "soilorgC", "poll", "rootBM", "plantCN")
 
 jena.dat <- 
   jena.dat %>%
@@ -68,16 +68,49 @@ hist(jena.dat$poll)
 lm.po <- glm(round(poll, 0) ~ sowndiv_scale, data = jena.dat, family = "poisson")
 plot(lm.po)
 
-summary(lm.po)
+lm.po.s <- summary(lm.po)
+lm.po.s
 
-# pollination
+# ratio should be approximately one: it is seven...
+lm.po.s$deviance/lm.po.s$df.residual
+lm.po.s$dispersion
+
+# load the DHARMa library
+sim_fmp <- simulateResiduals(lm.po, refit=F)
+testOverdispersion(sim_fmp)
+
+# what to do about overdispersion? Well we can fit a dispersion parameter.
+# https://biometry.github.io/APES/LectureNotes/2016-JAGS/Overdispersion/OverdispersionJAGS.pdf
+lm.po2 <- glm(round(poll, 0) ~ sowndiv_scale, data = jena.dat, family = quasipoisson)
+summary(lm.po2)
+
+
+# rootBN
 hist(jena.dat$rootBM)
-lm.po <- glm(sqrt(rootBM) ~ sowndiv_scale, data = jena.dat, family = "gaussian")
-plot(lm.po)
+lm.rb <- glm(sqrt(rootBM) ~ sowndiv_scale, data = jena.dat, family = "gaussian")
+plot(lm.rb)
 
-summary(lm.po)
+summary(lm.rb)
+
+# plantCN
+hist(jena.dat$plantCN)
+lm.cn <- glm(plantCN ~ sowndiv_scale, data = jena.dat, family = "gaussian")
+plot(lm.cn)
+
+summary(lm.cn)
 
 # generate a correlation plot between these functions
+library(GGally)
 
+jena.corr <- jena.dat[, func.names]
+names(jena.corr) <- c("Biomass", "Soil Org. C", "Pollination", "Root biomass", "C/N")
 
+ggcorr(jena.corr, method = c("everything", "pearson"), label = TRUE)
+
+cor(jena.corr)
+
+p.corr <- Hmisc::rcorr(as.matrix(jena.corr))
+p.out <- p.corr$P[lower.tri(p.corr$P, diag = FALSE)]
+
+p.adjust(p = p.out, method = "bonferroni")
 
