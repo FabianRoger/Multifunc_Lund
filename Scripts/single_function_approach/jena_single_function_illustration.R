601b64dcfcfab3cdc5e9dd7cfe20189d325dd5fb
James Hagan <james_hagan@outlook.com>
2021-11-08 12:01:09 +0100

    use a negative binomial distribution for pollination and plot a decent correlation plot

--- Scripts/single_function_approach/jena_single_function_illustration.R
+++ Scripts/single_function_approach/jena_single_function_illustration.R
@@ -8,6 +8,11 @@ library(here)
 library(readr)
 library(dplyr)
 library(ggplot2)
+library(DHARMa)
+library(MASS)
+library(corrr)
+library(tidyr)
+library(forcats)
 
 rm(list = ls())
 
@@ -75,15 +80,34 @@ lm.po.s
 lm.po.s$deviance/lm.po.s$df.residual
 lm.po.s$dispersion
 
-# load the DHARMa library
+# use the DHARMa library
 sim_fmp <- simulateResiduals(lm.po, refit=F)
 testOverdispersion(sim_fmp)
 
-# what to do about overdispersion? Well we can fit a dispersion parameter.
-# https://biometry.github.io/APES/LectureNotes/2016-JAGS/Overdispersion/OverdispersionJAGS.pdf
-lm.po2 <- glm(round(poll, 0) ~ sowndiv_scale, data = jena.dat, family = quasipoisson)
-summary(lm.po2)
+# what to do about overdispersion? 
+# 1. we can fit a dispersion parameter
+# 2. we can use the negative binomial which can model different variances
+# 3. we can use an observation-level random effect
 
+# https://biometry.github.io/APES/LectureNotes/2016-JAGS/Overdispersion/OverdispersionJAGS.pdf
+
+# 2. use a negative binomial distribution
+library(MASS)
+lm.po2 <- glm.nb(round(poll, 0) ~ sowndiv_scale, data = jena.dat, link = log)
+
+# check for overdispersion
+lm.po2$deviance/lm.po2$df.residual
+
+# graphical analysis of the residuals
+plot(lm.po2)
+
+# overdispersion test using the DHARMa library
+sim_fmp <- simulateResiduals(lm.po2, refit=F)
+testOverdispersion(sim_fmp)
+plot(sim_fmp)
+
+# run the summary function
+summary(lm.po2)
 
 # rootBN
 hist(jena.dat$rootBM)
@@ -100,17 +124,73 @@ plot(lm.cn)
 summary(lm.cn)
 
 # generate a correlation plot between these functions
-library(GGally)
-
-jena.corr <- jena.dat[, func.names]
+jena.corr.raw <- jena.dat[, func.names]
 names(jena.corr) <- c("Biomass", "Soil Org. C", "Pollination", "Root biomass", "C/N")
 
-ggcorr(jena.corr, method = c("everything", "pearson"), label = TRUE)
+jena.corr <-
+  jena.corr.raw %>%
+  corrr::correlate(diagonal = 1) %>%
+  corrr::shave(upper = FALSE)
 
-cor(jena.corr)
+# calculate the correlation and p-values using Hmisc
+p.corr <- Hmisc::rcorr(as.matrix(jena.corr.raw))
 
-p.corr <- Hmisc::rcorr(as.matrix(jena.corr))
-p.out <- p.corr$P[lower.tri(p.corr$P, diag = FALSE)]
+# extract a matrix of p-values
+p.out <- p.corr$P
 
-p.adjust(p = p.out, method = "bonferroni")
+# set the diagonals to 1
+diag(p.out) <- 1
+
+#replace the lower half with NAs to match the correlation
+p.out[lower.tri(p.out, diag = FALSE)] <- NA
+
+# correct the p-values using hocberg's correction for multiple comparisons
+p.out[ (p.out == p.out | is.na(p.out)) ] <- ifelse(p.adjust(p = p.out, method = "hochberg") < 0.05, "*", "") 
+
+# convert to a tibble
+p.out <- as_tibble(p.out)
+
+# prepare the correlation data for plotting
+jena.corr <- 
+  jena.corr %>%
+  pivot_longer(
+    cols = -term,
+    names_to = "colname",
+    values_to = "corr"
+  ) %>%
+  mutate(rowname = fct_inorder(term),
+         colname = fct_inorder(colname))
+
+# prepare the p-value data for plotting
+p.out$term <- row.names(p.corr$P) 
+
+p.out <- 
+  p.out %>%
+  pivot_longer(
+    cols = -term,
+    names_to = "colname",
+    values_to = "corr"
+  ) %>%
+  mutate(rowname = fct_inorder(term),
+         colname = fct_inorder(colname))
+
+ggplot(jena.corr, aes(rowname, fct_rev(colname),
+                 fill = corr)) +
+  geom_tile() +
+  geom_text(aes(
+    label = format(round(corr, 2), nsmall = 2),
+    color = abs(corr) < .75
+  )) +
+  geom_text(data = p.out, 
+            mapping = aes(label = corr)) +
+  coord_fixed(expand = FALSE) +
+  scale_color_manual(values = c("white", "black"),
+                     guide = "none") +
+  scale_fill_distiller(
+    palette = "PuOr", na.value = "white",
+    direction = 1, limits = c(-1, 1)
+  ) +
+  labs(x = NULL, y = NULL) +
+  theme(panel.border = element_rect(color = NA, fill = NA),
+        legend.position = c(.85, .8))
 
