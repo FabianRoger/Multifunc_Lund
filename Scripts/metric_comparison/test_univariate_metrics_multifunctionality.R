f993a472b08292201bda7e20412921edb11d5a23
James Hagan <james_hagan@outlook.com>
2021-11-09 14:59:10 +0100

    write a function to automate the simulations

--- Scripts/metric_comparison/test_univariate_metrics_multifunctionality.R
+++ Scripts/metric_comparison/test_univariate_metrics_multifunctionality.R
@@ -17,12 +17,31 @@ source(here("Scripts/MF_functions_collated.R"))
 
 # write a function to generate these function distributions
 
+univariate_explorer <- function(funcnum = 5, grain = 0.05, error = 0.01) {
+  
+  # generate n functions between 0 and 1
+  x <- replicate(funcnum, seq(0, 1, grain), simplify = FALSE)
+  
+  # put these functions into a matrix
+  y <- do.call("expand.grid", x)
+  
+  # add a small amount of error and set to zero if zero
+  nm <- nrow(y)*ncol(y)
+  y <- y + rnorm(n = nm, 0, error)
+  y[y < 0] <- 0
+  
+  names(y) <- paste("F_", 1:funcnum, sep = "")
+  
+  # convert y to a tibble
+  df <- tibble(y)
+  
+  # only get unique rows
+  df <- distinct(df)
+  
+}
 
-
-
-
-
-
+# simulate data for five ecosystem functions
+sim.dat <- univariate_explorer() 
 
 
 
