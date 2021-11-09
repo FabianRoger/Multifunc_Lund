b0eacf0b6b024f4c61a78985bbc853de9633c85c
James Hagan <james_hagan@outlook.com>
2021-11-09 14:17:36 +0100

    add a script to compare the different metrics of multifunctionality

---
+++ Scripts/metric_comparison/test_univariate_metrics_multifunctionality.R
@@ -0,0 +1,28 @@
+
+# Project: Review of multifunctionality in ecology, conservation and ecosystem service science
+
+# Title: Code to compare different metrics of multifunctionality given different function distributions
+
+# load relevant libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(here)
+
+rm(list = ls())
+
+# tell R where to call scripts from
+source(here("Scripts/function_plotting_theme.R"))
+source(here("Scripts/MF_functions_collated.R"))
+
+# write a function to generate these function distributions
+
+
+
+
+
+
+
+
+
+
