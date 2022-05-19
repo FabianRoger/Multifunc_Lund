7d3819cc58b2addc8eb1e6943a811687af618d9b
James Hagan <james_hagan@outlook.com>
2022-05-19 16:38:38 +0200

    try to figure out why the slope changes with the number of functions

--- Scripts/06_number_of_functions/5_analyse_n_funcs_effect_bio_mf.R
+++ Scripts/06_number_of_functions/5_analyse_n_funcs_effect_bio_mf.R
@@ -82,6 +82,10 @@ nfunc_slopes <-
   ungroup()
 View(nfunc_slopes)
 
+nfunc_slopes %>%
+  group_by(multifunctionality_metric, function_matrix) %>%
+  summarise(mean_slope = mean(number_of_functions_diversity_mf_est) )
+
 plots.fx2 <- vector("list", length = length(mf.metric.list))
 for(i in 1:length(mf.metric.list)) {
   
@@ -127,15 +131,17 @@ library(patchwork)
 
 f.x1 <- 
   plots.fx1$ave_MF + plots.fx2$ave_MF + 
-  plots.fx1$sd_MF + plots.fx2$sd_MF + 
+  plots.fx1$sum_MF + plots.fx2$sum_MF + 
+  # plots.fx1$sd_MF + plots.fx2$sd_MF + 
   plots.fx1$Pasari_MF + plots.fx2$Pasari_MF + 
+  # plots.fx1$ENF_MF + plots.fx2$ENF_MF + 
   plots.fx1$thresh_30_MF + plots.fx2$thresh_30_MF +
   plots.fx1$thresh_70_MF + plots.fx2$thresh_70_MF + 
   plot_layout(ncol = 2, widths = c(1, 2.5)) +
   plot_annotation(tag_levels = "a")
 f.x1
 
-ggsave(filename = here("Figures/fig_x1.png"), plot = f.x1, width = 18, height = 27,
+ggsave(filename = here("Figures/fig_x1.png"), plot = f.x1, width = 18, height = 35,
        units = "cm")
 
 
@@ -153,29 +159,95 @@ head(sim.n.sub)
 names(sim.n.sub)
 View(sim.n.sub)
 
+# analyse examples from the raw model data
+source(here("Scripts/01_general_functions/process_model_data.R"))
+
+# load the raw data
+mod.out <- read_csv(here("data/drift_model_n_functions_processed.csv"))
+head(mod.out)
+
+# get one random model from function matrix 2
+mod1 <- 
+  mod.out %>%
+  filter(function_matrix == 2)
+
+mod1 <- mod1[mod1$mod_id == sample(mod1$mod_id, 1), ]
+head(mod1)
+
+# write the adf.data into a new data.frame
+dat.in <- mod1
+
+# use the function to get a list of all function combinations
+list.func.names <- get.function.combinations(function.names = c("F_1", "F_2", "F_3", "F_4", "F_5"))
+
+# set an output list
+list.out <- vector("list", length(list.func.names))
+
+# loop over each set of function names
+for (i in 1:length(list.func.names)) {
+  
+  # get the vector of function names
+  sample.func.names <- list.func.names[[i]]
+  
+  # calculate the multifunctionality metrics
+  data.mf <- 
+    dat.in %>%
+    mutate(sum_MF = MF_sum(adf = dat.in, vars = sample.func.names),
+           ave_MF = MF_av(adf = dat.in, vars = sample.func.names, stand_method = "z_score_abs"),
+           sd_MF = MF_sd(adf = dat.in, vars = sample.func.names, stand_method = "z_score_abs"),
+           Pasari_MF = MF_pasari(adf = dat.in, vars = sample.func.names),
+           ENF_MF = hill_multifunc(adf = dat.in, vars = sample.func.names, scale = 1, HILL = TRUE),
+           thresh_30_MF = single_threshold_mf(adf = dat.in, vars = sample.func.names, thresh = 0.3),
+           thresh_70_MF  = single_threshold_mf(adf = dat.in, vars = sample.func.names, thresh = 0.7) 
+    ) %>%
+    mutate(n_func = length(sample.func.names),
+           func_id = paste(sample.func.names, collapse = ""))
+  
+  
+  data.mf$sd_func <- apply(dat.in[, sample.func.names], 1, sd)
+    
+  list.out[[i]] <- data.mf
+  
+  }
+
+bind_rows(list.out) %>%
+  select(n_func, func_id, local_species_pool, sd_func) %>%
+  distinct() %>%
+  mutate(n_func = as.character(n_func)) %>%
+  ggplot(data = .,
+         mapping = aes(x = local_species_pool, y = sd_func, colour = n_func)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  theme_meta()
+
+bind_rows(list.out) %>%
+  pivot_longer(cols = ends_with("_MF"),
+               names_to = "metric",
+               values_to = "MF") %>%
+  ggplot(data = .,
+         mapping = aes(x = local_species_pool, y = MF, 
+                       group = func_id, colour = as.character(n_func))) +
+  geom_jitter(alpha = 0.3) +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(~metric, scales = "free") +
+  theme_meta() +
+  theme(legend.position = "bottom")
+
+
 # Does the variation decline with species richness more with more functions?
 
 # Yes, with function matrix 2 i.e. different functional contributions of species
 ggplot(data = sim.n.sub,
        mapping = aes(x = number_of_functions, y = sd_cor) ) +
-  geom_jitter(width = 0.1) +
+  geom_jitter(width = 0.1, alpha = 0.2) +
   facet_wrap(~function_matrix) +
-  geom_hline(yintercept = 0, colour = "red") +
+  ylab("Spearman's rho(species richness ~ SD)") +
+  xlab("Number of functions") +
+  geom_hline(yintercept = 0, colour = "red", linetype = "dashed") +
   theme_meta()
 
-ggplot(data = sim.n.slopes,
-       mapping = aes(x = number_of_functions_sd_cor)) +
-  geom_histogram(alpha = 0.2) +
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  facet_wrap(~function_matrix, scales = "free_y") +
-  theme_meta()
-
-ggplot(data = sim.n.slopes,
-       mapping = aes(x = number_of_functions_mean_cor)) +
-  geom_histogram(alpha = 0.2) +
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  facet_wrap(~function_matrix, scales = "free_y") +
-  theme_meta()
+ggsave(filename = here("Figures/fig_S2.png"), width = 16, height = 9,
+       units = "cm")
 
 # does the range in multifunctionality change with more functions?
 ggplot(data = sim.n.out %>%
@@ -184,14 +256,43 @@ ggplot(data = sim.n.out %>%
   geom_jitter(width = 0.1, alpha = 0.2) +
   facet_wrap(~multifunctionality_metric, scales = "free") +
   geom_smooth(method = "lm", se = FALSE) +
+  xlab("Number of functions") +
+  ylab("Range of multifunctionality (max - min)") +
+  scale_colour_viridis_d(option = "C", end = 0.9) +
+  guides(colour = guide_legend(title="Function matrix")) +
+  geom_hline(yintercept = 0, colour = "black", linetype = "dashed") +
+  theme_meta() +
+  theme(legend.position = "top", 
+        legend.key=element_blank(),
+        legend.title = element_text(size = 11))
+
+ggsave(filename = here("Figures/fig_S3.png"), width = 18, height = 20,
+       units = "cm")
+
+ggplot(data = sim.n.out %>%
+         mutate(function_matrix = as.character(function_matrix)),
+       mapping = aes(x = number_of_functions, y = sd_MF, colour = function_matrix) ) +
+  geom_jitter(width = 0.1, alpha = 0.2) +
+  facet_wrap(~multifunctionality_metric, scales = "free") +
+  geom_smooth(method = "lm", se = FALSE) +
   geom_hline(yintercept = 0, colour = "red") +
   theme_meta()
 
+ggplot(data = sim.n.out %>%
+         mutate(function_matrix = as.character(function_matrix)),
+       mapping = aes(x = number_of_functions, y = min_MF, colour = function_matrix) ) +
+  geom_jitter(width = 0.1, alpha = 0.2) +
+  facet_wrap(~multifunctionality_metric, scales = "free") +
+  geom_smooth(method = "lm", se = FALSE) +
+  geom_hline(yintercept = 0, colour = "red") +
+  theme_meta()
 
+sim.n.out %>%
+  filter(range_MF == 0) %>%
+  View()
 
+sim.n.out %>%
+  filter(multifunctionality_metric == "sd_MF") %>%
+  View()
 
-
-
-
-
-
+### END
