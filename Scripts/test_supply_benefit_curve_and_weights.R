6779ec4d2df6517921de6beb6d567218f13da3df
James Hagan <james_hagan@outlook.com>
2022-09-29 13:00:56 +0200

    test the supply-benefit curves

--- Scripts/test_supply_benefit_curve_and_weights.R
+++ Scripts/test_supply_benefit_curve_and_weights.R
@@ -8,9 +8,14 @@ source(here("Scripts/01_general_functions/function_plotting_theme.R"))
 source(here("Scripts/01_general_functions/MF_functions_collated.R"))
 
 # simulate different functions
+
+# number of functions
 nf <- 5
+
+# number of samples
 reps <- 100
 
+# draw functions from a normal distribution with mean of 10 and an sd of 2
 mf_out <- vector("list", length = nf)
 for (i in 1:nf) {
   
@@ -22,11 +27,16 @@ for (i in 1:nf) {
   
 }
 
+# bind these functions into a data.frame and rename
 mf_out <- bind_cols(mf_out)
 names(mf_out) <- paste0("F_", 1:nf)
 
+# print the data.frame
+print(mf_out)
 
 # (1) define a supply-benefit curve for each function
+
+# write functions for different types of supply benefit curve
 
 # threshold supply-benefit
 thresh <- function(x, thresh = 0.5) {
@@ -61,6 +71,8 @@ exponential <- function(x) {
 
 # choose supply-benefit curves for each function
 sb_curves <- c("linear", "linear", "thresh", "step_linear", "exponential")
+
+# choose additional arguments for each of the sb_curves
 sb_args <- list(NA, NA, 0.5, 0.3, NA)
 
 # apply the different supply-benefit functions
@@ -80,8 +92,10 @@ for(i in 1:ncol(mf_out)) {
   
 }
 
-
 # (2) transform to a common scale or not?
+
+# if our supply-benefit curves convert to a benefit score between 0 and 1, we don't need this step
+# likewise, if our supply benefit curves convert to a monetary value score, we also don't need this step
 
 # transform Y-N
 transform <- "Y"
@@ -90,7 +104,6 @@ transform <- "Y"
 method = "max"
 
 # apply the standardisation
-
 if (transform == "Y") {
   
   mf_out <- 
@@ -102,20 +115,23 @@ if (transform == "Y") {
 # (3) apply weights to each function
 
 # set-up the weights
-ws <- c(1, 0.1, 0.5, 3, 1)
+
+# these numbers represent how we might value the functions relative to each other
+ws <- c(1, 0.1, 0.5, 3, 5)
+
+# these are then scaled to the number of functions
 ws <- ( (ws/sum(ws))*nf )
 
+# calculate summed multifunctionality without weights
 mf <- apply(mf_out, 1, sum)
+range(mf)
 
+# calculate summed multifunctionality with weights
 x <- sweep(mf_out, MARGIN = 2, ws, `*`)
 x1 <- apply(x, 1, sum)
+range(x1)
 
+# compare multifunctionality with and without weights
 plot(mf, x1)
 
-
-
-
-
-
-
-
+### END
